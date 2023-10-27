753391e61062588a2d329bd55db8936ef18cb7d6
prof-ronny <carlos.sousa34@fatec.sp.gov.br>
2023-10-27 16:27:01 -0300

    Navegação de telas

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './Home';
+import AppNavigator from './AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Home></Home>
-
-    </View>
+    <AppNavigator/>
   );
 }
 
