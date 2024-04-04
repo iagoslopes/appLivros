167c6da00c5a4b1a43b07118731552b46840fbbd
Iago de Souza Lopes <128859658+iagoslopes@users.noreply.github.com>
2024-04-04 00:43:37 +0000

    Mudança ViewLivro

--- screens/ViewLivro.tsx
+++ screens/ViewLivro.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import axios from 'axios';
 
 export default function ViewLivro({ route }) {
@@ -20,16 +20,94 @@ export default function ViewLivro({ route }) {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       {livro && (
-        <>
-          <Text>{livro.titulo}</Text>
-          <Text>{livro.ano}</Text>          
-          <Text>{livro.editora}</Text>
-          <Text>{livro.autorPrincipal }</Text>
-          <Image source={{ uri: livro.imagem}} style={{ width: 100, height: 100 }} />
-        </>
+        <View style={styles.content}>
+          <View style={styles.gradient}></View>
+        
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: livro.imagem}} style={{ width:150, height:200 }} />
+            <Text style={styles.title}>{livro.titulo}</Text>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.label}>Ano</Text>
+            <Text style={styles.info}>{livro.ano}</Text>
+
+            <Text style={styles.label}>Editora</Text>
+            <Text style={styles.info}>{livro.editora}</Text>
+
+            <Text style={styles.label}>Autor Principal</Text>
+            <Text style={styles.info}>{livro.autorPrincipal}</Text>
+
+            <Text style={styles.label}>Autores</Text>
+            <Text style={styles.info}>{livro.autores}</Text>
+
+            <Text style={styles.label}>Edição</Text>
+            <Text style={styles.info}>{livro.edicao}</Text>
+
+            <Text style={styles.label}>Idioma</Text>
+            <Text style={styles.info}>{livro.idioma}</Text>
+
+            <Text style={styles.label}>ISBN/ISSN</Text>
+            <Text style={styles.info}>{livro.isbnIssn}</Text>
+
+            <Text style={styles.label}>Material</Text>
+            <Text style={styles.info}>{livro.material}</Text>
+
+            <Text style={styles.label}>Obra</Text>
+            <Text style={styles.info}>{livro.obra}</Text>    
+          </View>
+        </View>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white', // Background branco para preencher a tela
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  imageContainer: {
+    alignItems: 'center', // Centraliza a imagem e o título
+    marginTop: 25,
+    marginBottom: 20, // Espaçamento abaixo da imagem e do título
+  },
+  gradient: {
+    position: 'absolute',
+    height: 200,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundImage: 'radial-gradient(circle, rgba(128, 128, 128, 1) 0%, rgba(32, 32, 32, 1) 70%)', // Cor de fundo do gradiente
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black', // Título em preto
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  infoContainer: {
+    alignItems: 'flex-start', // Alinha os outros campos à esquerda
+    marginLeft: 20, // Adiciona espaçamento à esquerda para os campos
+    marginRight: 20, // Alinha os outros campos à esquerda
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 1,
+  },
+  info: {
+    fontSize: 15,
+    color: 'black', // Textos em preto
+    marginBottom: 7,
+    textAlign: 'justify',
+  },
+});
